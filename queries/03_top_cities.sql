2859c2c3755015d48eb41e9f62bcb50f89e7b224
dilarakarabacak-data <dilarakarabacak.01@hotmail.com>
2026-04-01 01:50:10 +0300

    Update 03_top_cities.sql

--- queries/03_top_cities.sql
+++ queries/03_top_cities.sql
@@ -1,2 +1,3 @@
 --Calculates top 3 most profitable cities
-select top 3 c.city,sum(p.payment_amount) sum_payment from customers c join orders o on c.customer_id=o.customer_id join payments p on o.order_id=p.order_id where o.status='Completed' group by c.city order by sum(p.payment_amount) desc
+select top 3 c.city,sum(p.payment_amount) sum_payment from customers c join orders o on c.customer_id=o.customer_id join payments p on o.order_id=p.order_id 
+where o.status='Completed' group by c.city order by sum(p.payment_amount) desc
