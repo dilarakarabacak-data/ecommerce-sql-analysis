ccce29dc36278ff1b025530a77412bee7cec0474
dilarakarabacak-data <dilarakarabacak.01@hotmail.com>
2026-04-01 01:51:25 +0300

    Update 01_repeat_purchase.sql

--- queries/01_repeat_purchase.sql
+++ queries/01_repeat_purchase.sql
@@ -1,1 +1,2 @@
-select count(customer_id)*1.0/(select count(*) from customers) as repeat_purchase_rate from (select c.customer_id from customers c join orders o on c.customer_id=o.customer_id group by c.customer_id having count(o.order_id)>1) x
+select count(customer_id)*1.0/(select count(*) from customers) as repeat_purchase_rate 
+from (select c.customer_id from customers c join orders o on c.customer_id=o.customer_id group by c.customer_id having count(o.order_id)>1) x
